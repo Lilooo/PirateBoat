cd99558f1c8e259b10881facf3afbe3d894b4432
Lilooo <alice.giordani@gmail.com>
2012-06-16 19:10:07 +0200

    XIb changes
    New class for torrent's details

---
+++ PirateBoat/DetailTorrentViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailTorrentViewController.h
+//  PirateBoat
+//
+//  Created by Alice Giordani on 16/06/12.
+//  Copyright (c) 2012 Dev0rAlive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailTorrentViewController : UIViewController
+
+@end
