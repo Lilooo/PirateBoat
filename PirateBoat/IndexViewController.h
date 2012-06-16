6ca5c0b4f4b744dbaa1e0cfb0c3ea25f5181f43d
Nicolas Dufreche <n.dufreche@gmail.com>
2012-06-16 18:08:14 +0200

    creation of the project architecture

---
+++ PirateBoat/IndexViewController.h
@@ -0,0 +1,13 @@
+//
+//  IndexViewController.h
+//  PirateBoat
+//
+//  Created by Nicolas Dufreche on 10/06/12.
+//  Copyright (c) 2012 Dev0rAlive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IndexViewController : UIViewController
+
+@end
