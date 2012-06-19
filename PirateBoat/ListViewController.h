7bb31398e45a00a68da160b22d926c2a2da6aa2b
Lilooo <alice.giordani@gmail.com>
2012-06-19 21:24:32 +0200

    Torrent model

---
+++ PirateBoat/ListViewController.h
@@ -0,0 +1,13 @@
+//
+//  ListViewController.h
+//  PirateBoat
+//
+//  Created by Alice Giordani on 19/06/12.
+//  Copyright (c) 2012 Dev0rAlive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ListViewController : UITableViewController
+
+@end
